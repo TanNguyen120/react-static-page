0c4cd8e64d816ae8c753e9717cfd1ab872bd95c3
Tan Nguyen <tannguyen1220@gmail.com>
2022-05-11 06:38:59 +0700

    react create app

---
+++ static-react-base/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Thing I like About Yogg Sharon</h1>
+        <ol>
+          <li> He an Old God</li>
+          <li> Have Beautiful teeth</li>
+          <li> TENTACLES</li>
+          <li>And Most Important RANDOM</li>
+        </ol>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
