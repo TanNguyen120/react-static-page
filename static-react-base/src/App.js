bbbc3426f468b62e5ae340515ef47267b9f736b1
Tan Nguyen <tannguyen1220@gmail.com>
2022-05-11 07:54:03 +0700

    change style

--- static-react-base/src/App.js
+++ static-react-base/src/App.js
@@ -16,21 +16,35 @@ function ListAboutYogg() {
   )
 }
 
+function Header() {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      {/* <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a> */}
+      <nav>
+        <ul className="nav-item">
+          <li> <a href="#">Home</a></li>
+          <li> <a href="#">About</a></li>
+          <li> <a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ListAboutYogg />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <ListAboutYogg />
+
     </div>
   );
 }
