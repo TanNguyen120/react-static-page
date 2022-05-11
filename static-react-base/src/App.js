2acbf9ef42853b617210a15fcac8602b221a0727
Tan Nguyen <tannguyen1220@gmail.com>
2022-05-11 07:20:53 +0700

    custom component

--- static-react-base/src/App.js
+++ static-react-base/src/App.js
@@ -1,19 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+function ListAboutYogg() {
+  return (
+    <div>
+      <h1>Thing I like About Yogg Sharon</h1>
+      <ol>
+        <li> He an Old God</li>
+        <li> Have Beautiful teeth</li>
+        <li> TENTACLES</li>
+        <li>And Most Important RANDOM</li>
+        <li>He some time will stab you in the back</li>
+      </ol>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Thing I like About Yogg Sharon</h1>
-        <ol>
-          <li> He an Old God</li>
-          <li> Have Beautiful teeth</li>
-          <li> TENTACLES</li>
-          <li>And Most Important RANDOM</li>
-        </ol>
-
+        <ListAboutYogg />
         <a
           className="App-link"
           href="https://reactjs.org"
