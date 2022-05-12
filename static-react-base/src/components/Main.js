2d6671e4b8bbd99e3c5ffd2535af6c9d7d8ff630
Tan Nguyen <tannguyen1220@gmail.com>
2022-05-12 09:40:17 +0700

    header section

--- static-react-base/src/components/Main.js
+++ static-react-base/src/components/Main.js
@@ -1,6 +1,15 @@
 function Main() {
     return (
-        <h1>main content goes here</h1>
+        <div className="main-div">
+            <h1 className="main-h1">Thing I like About Yogg Sharon</h1>
+            <ol className="main-ol">
+                <li> He an Old God</li>
+                <li> Have Beautiful teeth</li>
+                <li> TENTACLES</li>
+                <li>And Most Important RANDOM</li>
+                <li>He some time will stab you in the back</li>
+            </ol>
+        </div>
     )
 }
 
--- static-react-base/src/components/NavBar.js
+++ static-react-base/src/components/NavBar.js
@@ -1,6 +1,12 @@
+import logo from '../logo.svg';
+
 function NavBar() {
     return (
-        <h1>nav bar goes here</h1>
+        <nav className="nav-item">
+            <img src={logo} width="100px"></img>
+            <h3>Yogg Sharon</h3>
+            <h4>React Course - project 1</h4>
+        </nav>
     )
 }
 
